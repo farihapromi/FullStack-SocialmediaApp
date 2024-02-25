3328631cc9b97983e649c20fe0b94abcd65d88a2
Fariha Promi <farihapromi2611@gmail.com>
2024-02-26 01:15:30 +0600

    social-media

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,11 @@
+import { z } from "zod"
+
+
+ export const signupValidation = z.object({
+    name:z.string().min(2,{message:'too short'}),
+
+    username: z.string().min(2).max(50),
+    email:z.string().email(),
+    password:z.string().min(8,{message:'password must be 8 characters '})
+  })
+  
